070c8e3057f6df67bebf6e9244752017b5a73555
Carlos Ramos <cramos-2018521@kinal.edu.gt>
2024-07-21 19:49:57 -0600

    Agregar archivos de proyecto.

---
+++ CSharp_Tabla_de_multiplicar/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class MultiplicationTable
+{
+    static void Main()
+    {
+        Console.Write("Ingrese un número: ");
+        int number = int.Parse(Console.ReadLine());
+
+        PrintMultiplicationTable(number);
+    }
+
+    static void PrintMultiplicationTable(int number)
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"{number} x {i} = {number * i}");
+        }
+    }
+}
